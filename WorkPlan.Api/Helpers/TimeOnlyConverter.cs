31d38b1f87e276df1f35638cff1bb6877b5b8a4f
eris.adhami <eri.adhami@gmail.com>
2022-10-09 20:14:20 +0200

    HELPERS: Added TimeOnlyConverter

---
+++ WorkPlan.Api/Helpers/TimeOnlyConverter.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace WorkPlan.Api.Helpers;
+
+public class TimeOnlyConverter : ValueConverter<TimeOnly, TimeSpan>
+{
+    public TimeOnlyConverter() : base(
+            timeOnly => timeOnly.ToTimeSpan(),
+            timeSpan => TimeOnly.FromTimeSpan(timeSpan))
+    {
+    }
+}
