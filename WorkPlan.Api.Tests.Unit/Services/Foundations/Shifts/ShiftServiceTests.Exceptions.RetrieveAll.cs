c56c41279b8fe748f634f8d9c0fa7a8275a89d8d
eris.adhami <eri.adhami@gmail.com>
2022-10-10 16:51:23 +0200

    ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogIt -> FAIL

--- WorkPlan.Api.Tests.Unit/Services/Foundations/Shifts/ShiftServiceTests.Exceptions.RetrieveAll.cs
+++ WorkPlan.Api.Tests.Unit/Services/Foundations/Shifts/ShiftServiceTests.Exceptions.RetrieveAll.cs
@@ -43,4 +43,42 @@ public partial class ShiftServiceTests
         this.storageBrokerMock.VerifyNoOtherCalls();
         this.loggingBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public void ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogIt()
+    {
+        //given
+        string exceptionMessage = GetRandomMessage();
+        var serviceException = new Exception(exceptionMessage);
+
+        var failedShiftServiceException =
+            new FailedShiftServiceException(serviceException);
+
+        var expectedShiftServiceException =
+            new ShiftServiceException(failedShiftServiceException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.SelectAllShifts())
+                .Throws(serviceException);
+
+        //when
+        Action retrieveAllShiftsAction = () =>
+                this.shiftService.RetrieveAllShifts();
+
+        //then
+        Assert.Throws<ShiftServiceException>(
+            retrieveAllShiftsAction);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectAllShifts(),
+                Times.Once);
+
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(
+                expectedShiftServiceException))),
+                    Times.Once);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
 }
